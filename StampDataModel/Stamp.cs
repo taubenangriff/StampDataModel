e1c4d546b4823a8ade8975adf9376e89013e1ca1
taubenangriff <51975164+taubenangriff@users.noreply.github.com>
2023-04-08 14:19:33 +0200

    Projektdateien hinzufügen.

---
+++ StampDataModel/Stamp.cs
@@ -0,0 +1,43 @@
+﻿namespace StampDataModel
+{
+    public class Stamp
+    {
+        public int StampPath { get; set; }
+        public int IconGUID { get; set; }
+        public List<Building>? BuildingInfo { get; set; }
+        public int? StreetCount { get; set; }
+        public List<(int, List<StreetPoint>)>? StreetInfo { get; set; }
+        public List<RailwaySegment>? RailwayInfos { get; set; }
+    }
+
+    public class Building
+    {
+        public float[] Pos { get; set; }
+        public float Dir { get; set; }
+        public int GUID { get; set; }
+        public long? ComplexOwnerID { get; set; }
+    }
+
+    public class StreetPoint
+    {
+        public StreetPoint() { }
+
+        public StreetPoint(float x, float y)
+        {
+            None = (x, y);
+        }
+        public (float, float) None { get; set; }
+    }
+
+    public class RailwaySegment
+    { 
+        public RailwaySegment() { }
+
+        public RailwaySegment(int fromX, int fromY, int toX, int toY) 
+        {
+            None = (new int[2] { fromX, fromY }, new int[2] { toX, toY });
+        }
+        public (int[], int[]) None { get; set; }
+    }
+
+}
---
+++ StampSerializingTest/Program.cs
@@ -0,0 +1,56 @@
+﻿using FileDBSerializing.ObjectSerializer;
+using StampDataModel;
+
+using (var fs = File.OpenRead("stamp.filedb"))
+{
+    var newstamp = FileDBConvert.DeserializeObject<Stamp>(fs, new FileDBSerializerOptions()
+    {
+        IgnoreMissingProperties = true
+    });
+
+    int i = 0; 
+}
+
+var streetEntries = new List<StreetPoint>
+{
+    new StreetPoint(1, 5),
+    new StreetPoint(5, 3)
+};
+var stamp = new Stamp()
+{
+    StampPath = 5000001,
+    IconGUID = 101325,
+    BuildingInfo = new List<Building>()
+    {
+        new Building() {
+            Pos = new float[2]{ 5, 3 },
+            Dir = 1.4f, 
+            GUID = 1337
+        },
+        new Building() {
+            Pos = new float[2]{ 9, 2 },
+            Dir = 1.4f,
+            GUID = 1337
+        }
+    },
+    StreetInfo = new()
+    {
+        (101308, streetEntries)
+    },
+    StreetCount = 5,
+    RailwayInfos = new List<RailwaySegment>()
+    { 
+        new RailwaySegment(1,5,2,6),
+        new RailwaySegment(9,9,5,3),
+        new RailwaySegment(9,11,9,11)
+    },
+
+};
+
+using (var fs = File.Create("stamp"))
+{
+    FileDBConvert.SerializeObject(stamp, new FileDBSerializerOptions()).CopyTo(fs);
+
+}
+
+    
